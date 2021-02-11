5c593a836ad69ccd53d811998479bf0a443e1cb4
Coffee Coder <coffeecoderpt@gmail.com>
2021-02-10 17:48:07 -0700

    Adds Greeting Program

---
+++ Basics/greeting.py
@@ -0,0 +1,21 @@
+# Say hello
+print("Hello.")
+
+# Ask for name
+name = input("What is your name? ")
+
+# Formal greeting
+print(f"Hi, {name}, it's very nice to meet you.")
+
+# Ask for age.
+age = input(f"How old are you {name}? ")
+
+# Acknowlege age and say the next year
+newAge = int(age) + 1
+
+print(f"{age} isn't a bad year. I hear {newAge} isn't a bad year either.")
+
+# Say goodbye
+print(f"I've had a great time {name}. I hope you have a great rest of your day.")
+
+input('')
